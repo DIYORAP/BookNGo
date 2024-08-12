3f714d39fb72c68e269ec72ade5f2656c61e5c91
Parthik Diyora <ddiyora503@gmail.com>
2024-08-12 15:51:55 +0530

    ticket booking backent 1..

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -119,13 +119,13 @@ export default function Example() {
                                         href={post.category.href}
                                         className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
                                     >
-                                        {post.category.title}
-                                    </a>
-                                </div>
+                                href="#"
+                                className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                            >
                                 <div className="group relative">
                                     <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                                        <a href={post.href}>
-                                            <span className="absolute inset-0" />
+                                <a href="#">
+                                    <span className="absolute inset-0" />
                                             {post.title}
                                         </a>
                                     </h3>
