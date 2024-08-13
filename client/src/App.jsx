3ac42e7fb6c41a36709f77519fad887eb430c353
Parthik Diyora <ddiyora503@gmail.com>
2024-08-13 17:08:26 +0530

    mnage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,16 +8,12 @@ import Example from "./pages/Home";
 import Profile from "./pages/Profile";
 import PrivateRoute from "./components/PrivateRoute";
 import EventVisitor from "./pages/EventVisitor";
-import CartComponent from "./pages/card/item";
-
 function App() {
 
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/ok" element={<CartComponent />} />
-
         <Route path="/" element={<Example />} />
         <Route path="/in" element={<EventVisitor />} />
         <Route path='/sign-up' element={<SignUp />} />
