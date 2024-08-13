9941059a8e3cca7e03a8cd6931315eb8e65893fe
Parthik Diyora <ddiyora503@gmail.com>
2024-08-13 21:44:27 +0530

    card creting price

--- client/src/pages/EventVisitor.jsx
+++ client/src/pages/EventVisitor.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Carousel from './visiteve/Carousel'
 import Eveinfo from './visiteve/Eveinfo'
+import { useState } from 'react';
 
 function EventVisitor() {
+    // const [events, setEvents] = useState([]);
+
+    // useEffect(() => {
+    //     const fetchEvents = async () => {
+    //         try {
+    //             const res = await fetch("/api/event/show/:id");
+    //             if (res.ok) {
+    //                 const data = await res.json();
+    //                 setEvents(data); // 
+    //             } else {
+    //                 console.error('Failed to fetch events');
+    //             }
+    //         } catch (error) {
+    //             console.error('Error fetching events:', error);
+    //         }
+    //     };
+    //     fetchEvents();
+    // }, []);
+
     return (
         <div>
             <Carousel />
